0ececfebaf123ce56b31ad364a130219e34eab7a
huongbls <huongntFX16409@funix.edu.vn>
2022-07-23 23:37:17 +0700

    Lấy dữ liệu

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   host: "localhost",
-  username: "root",
+  user: "root",
   database: "node-complete",
   password: "@Abc123456789",
 });
