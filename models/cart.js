22161984afc731035e038e85e5982c46fdd06ec8
huongbls <huongntFX16409@funix.edu.vn>
2022-07-23 14:15:13 +0700

    Xóa các mục trong Giỏ hàng

--- models/cart.js
+++ models/cart.js
@@ -1,7 +1,11 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const p = path.join(path.dirname(require.main.filename), "data", "cart.json");
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'cart.json'
+);
 
 module.exports = class Cart {
   static addProduct(id, productPrice) {
@@ -13,7 +17,7 @@ module.exports = class Cart {
       }
       // Analyze the cart => Find existing product
       const existingProductIndex = cart.products.findIndex(
-        (prod) => prod.id === id
+        prod => prod.id === id
       );
       const existingProduct = cart.products[existingProductIndex];
       let updatedProduct;
@@ -28,7 +32,7 @@ module.exports = class Cart {
         cart.products = [...cart.products, updatedProduct];
       }
       cart.totalPrice = cart.totalPrice + +productPrice;
-      fs.writeFile(p, JSON.stringify(cart), (err) => {
+      fs.writeFile(p, JSON.stringify(cart), err => {
         console.log(err);
       });
     });
@@ -40,14 +44,15 @@ module.exports = class Cart {
         return;
       }
       const updatedCart = { ...JSON.parse(fileContent) };
-      const product = updatedCart.products.find((prod) => prod.id === id);
+      const product = updatedCart.products.find(prod => prod.id === id);
       const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter(
-        (prod) => prod.id !== id
+        prod => prod.id !== id
       );
       updatedCart.totalPrice =
         updatedCart.totalPrice - productPrice * productQty;
-      fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
         console.log(err);
       });
     });
