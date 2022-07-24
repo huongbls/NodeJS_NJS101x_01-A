82ddb610dff6c0a25436144ea6d7ad78ed366d02
huongbls <huongntFX16409@funix.edu.vn>
2022-07-24 13:45:04 +0700

    Kết nối với Cơ sở dữ liệu

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "root", "@Abc123456789", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "@Abc123456789",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
