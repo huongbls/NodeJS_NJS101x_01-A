ef49a54a6d9b98e40f711b6df657fa0c0625baef
huongbls <huongntFX16409@funix.edu.vn>
2022-07-24 22:06:23 +0700

    Đồng bộ hóa các định nghĩa JS với Cơ sở dữ liệu

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 const app = express();
 
@@ -14,13 +14,13 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-db.execute("select * from products")
-  .then((result) => {
-    console.log(result[0], result[1]);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// db.execute("select * from products")
+//   .then((result) => {
+//     console.log(result[0], result[1]);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -30,4 +30,10 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then((result) => {
+    console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
