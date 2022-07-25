1cf39e04fc7fc057676acc56d80cf68a021f53ed
huongbls <huongntFX16409@funix.edu.vn>
2022-07-25 15:36:03 +0700

    Thêm các sản phẩm hiện có & lấy các mặt hàng trong giỏ hàng

--- controllers/shop.js
+++ controllers/shop.js
@@ -75,6 +75,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   let fetchedCart;
+  let newQuantity = 1;
   req.user
     .getCart()
     .then((cart) => {
@@ -86,22 +87,24 @@ exports.postCart = (req, res, next) => {
       if (products.length > 0) {
         product = products[0];
       }
-      let newQuantity = 1;
+
       if (product) {
-        //...
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
       }
-      return Product.findByPk(prodId)
-        .then((product) => {
-          return fetchedCart.addProduct(product, {
-            through: { quantity: newQuantity },
-          });
-        })
-        .then(() => {
-          res.redirect("/cart");
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      return Product.findByPk(prodId);
+    })
+    .then((product) => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity },
+      });
+    })
+    .then(() => {
+      res.redirect("/cart");
+    })
+    .catch((err) => {
+      console.log(err);
     })
     .catch((err) => {
       console.log(err);
