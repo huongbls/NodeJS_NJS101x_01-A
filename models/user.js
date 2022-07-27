c04629287aace6d457f3ef9c746ce252caed1e98
huongbls <huongntFX16409@funix.edu.vn>
2022-07-27 22:58:29 +0700

    Nhận đơn đặt hàng

--- models/user.js
+++ models/user.js
@@ -101,8 +101,12 @@ class User {
       });
   }
 
-  getOrder() {
+  getOrders() {
     const db = getDb();
+    return db
+      .collection("orders")
+      .find({ "user._id": new ObjectId(this._id) })
+      .toArray();
   }
 
   static findById(userId) {
