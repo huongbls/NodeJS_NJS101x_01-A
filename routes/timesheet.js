d0275e27b8dd64b7004d72370c78f9e5be0a088a
huongbls <huongntFX16409@funix.edu.vn>
2022-08-05 08:48:50 +0700

    checkin

--- routes/timesheet.js
+++ routes/timesheet.js
@@ -5,6 +5,11 @@ const router = express.Router();
 const CheckIn = require("../models/CheckIn");
 const Employee = require("../models/Employee");
 
+//Thu nghiem
+// router.get("/timesheet/checkIn", (req, res) => {
+//   res.send("Day la router employee");
+// });
+
 // CheckIn.find()
 //   .populate("employeeId")
 //   .then((data) => {
@@ -14,11 +19,6 @@ const Employee = require("../models/Employee");
 //     console.log(err);
 //   });
 
-//Thu nghiem
-// router.get("/timesheet/checkIn", (req, res) => {
-//   res.send("Day la router employee");
-// });
-
 //Hiển thị thông tin checkIn
 router.get("/", async (req, res, next) => {
   const detailCheckIn = await CheckIn.find().lean().sort({ date: -1 });
@@ -27,10 +27,18 @@ router.get("/", async (req, res, next) => {
 
 // Hiện thị form để tạo bài viết mới
 router.get("/check-in", (req, res, next) => {
-  res.render("timesheet/checkIn");
+  CheckIn.find()
+    .populate("employeeId")
+    .then((data) => {
+      console.log(data);
+      res.render("timesheet/checkIn");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-//Tạo post mới
+//Tạo checkin mới
 router.post("/", async (req, res, next) => {
   console.log(req.employee._id);
   const employeeId = req.employee._id;
@@ -38,9 +46,16 @@ router.post("/", async (req, res, next) => {
   const newCheckInData = { employeeId, workingPlace, date };
   const newCheckIn = new CheckIn(newCheckInData);
   await newCheckIn.save();
+  // await newCheckIn.find().populate("employeeId").save();
+  await CheckIn.find().populate("employeeId");
+  // .then((data) => {
+  //   console.log(data);
+  //   res.render("timesheet/checkIn");
+  // })
+  // .catch((err) => {
+  //   console.log(err);
+  // });
   res.redirect("/timesheet");
 });
-
-CheckIn.find().populate("employeeId");
 
 module.exports = router;
