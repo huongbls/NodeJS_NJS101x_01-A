caf6501f1bb1784acf5313ee2231b862cb63de45
huongbls <huongntFX16409@funix.edu.vn>
2022-08-15 16:36:28 +0700

    Thao tác Cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,11 +3,11 @@ exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: req.isLoggedIn,
+    isAuthenticated: isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
   res.redirect("/");
 };
