167192f91994687a7e8403d0e4f6f5efaad18d5e
huongbls <huongntFX16409@funix.edu.vn>
2022-08-23 23:36:02 +0700

    Lọc tập tin bằng Mimetype

--- app.js
+++ app.js
@@ -30,16 +30,30 @@ const fileStorage = multer.diskStorage({
     cb(null, "image");
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + "-" + file.originalname);
+    cb(null, new Date().toISOString().slice(0, 10) + "-" + file.originalname);
   },
 });
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage }).single("image"));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
