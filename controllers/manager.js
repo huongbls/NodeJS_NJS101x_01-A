2b5ea67852097bf98cb11b364d63e82f44a4f460
huongbls <huongntFX16409@funix.edu.vn>
2022-09-10 14:27:32 +0700

    approve timesheet

--- controllers/manager.js
+++ controllers/manager.js
@@ -22,7 +22,6 @@ exports.getApproveTimesheet = async (req, res, next) => {
 exports.getApproveTimesheetSearch = async (req, res, next) => {
   let staffTimesheet = [];
   const searchMonth = req.query.searchMonth;
-  const searchEmployee = req.query.searchEmployee;
   const deptMembers = await User.find({
     department: req.user.department,
     position: "staff",
@@ -35,30 +34,31 @@ exports.getApproveTimesheetSearch = async (req, res, next) => {
   );
 
   deptMembers.forEach((member) => {
-    if (member.name === searchEmployee) {
-      staffTimesheet.push({
-        _id: member._id,
-        name: member.name,
-      });
-      const user = new User(member);
-      user.getStatistic().then((statistic) => {
-        statistic.forEach((x) => {
-          if (x.date >= firstDayOfMonth && x.date <= lastDayOfMonth) {
-            staffTimesheet.push(x);
-          }
-        });
-        res.render("manager/approve-timesheet", {
-          member: deptMembers,
-          user: req.session.user,
-          searchMonth: searchMonth,
-          searchEmployee: searchEmployee,
-          staffTimesheet: staffTimesheet,
-          active: { approve: true },
-          isAuthenticated: req.session.isLoggedIn,
-          manager: req.user.position === "manager" ? true : false,
+    let workingRecord = [];
+    const user = new User(member);
+    user.getStatistic().then((statistic) => {
+      statistic.forEach((x) => {
+        if (x.date >= firstDayOfMonth && x.date <= lastDayOfMonth) {
+          workingRecord.push(x);
+        }
+        staffTimesheet.push({
+          _id: member._id,
+          name: member.name,
+          workingRecord: workingRecord,
         });
       });
-    }
+      console.log(staffTimesheet);
+      res.render("manager/approve-timesheet", {
+        member: deptMembers,
+        user: req.session.user,
+        searchMonth: searchMonth,
+        staffTimesheet: staffTimesheet,
+        active: { approve: true },
+        isAuthenticated: req.session.isLoggedIn,
+        manager: req.user.position === "manager" ? true : false,
+      });
+    });
+    // console.log(staffTimesheet);
   });
 };
 
