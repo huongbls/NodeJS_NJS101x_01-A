c010b1acd58fa264d4e350cd528f81d481ff4e6b
huongbls <huongntFX16409@funix.edu.vn>
2022-09-23 14:49:44 +0700

    Lab21_1

---
+++ server/app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+const cors = require("cors");
+
+const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuidv4() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET",
+    "PATCH",
+    "POST",
+    "PUT",
+    "DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+mongoose
+  .connect(
+    "mongodb+srv://test:Nj28g2UmFeAYdDAe@funixnjs101xcluster.hlmi1es.mongodb.net/message?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    console.log("Connect to DB");
+    app.listen(8080, () => {
+      console.log("Server is running at port 8080");
+    });
+  })
+  .catch((err) => console.log(err));
